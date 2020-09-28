10dcea740959b65ee1a5bd2b4cd714e0ed09c48f
RiseToOp <72051257+RiseToOp@users.noreply.github.com>
2020-09-29 00:13:11 +0100

    Rise.php

--- src/SexyLush/TagsUI/Main.php
+++ src/SexyLush/TagsUI/Main.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SexyLush\TagsUI;
+namespace Misttakennnn\TagsUI;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
@@ -63,7 +63,7 @@ class Main extends PluginBase {
                             return true;
                 case 1: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.alien")) {
+                            if (!$player->hasPermission("tags.God")) {
                                 $player->sendMessage("§7§l(§c!§7)§r§f This tag is locked!");
                                 return true;
                             }else{
@@ -78,7 +78,7 @@ class Main extends PluginBase {
                             return true;
                 case 2: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.bombom")) {
+                            if (!$player->hasPermission("tags.Bad")) {
                                 $player->sendMessage("§7§l(§c!§7)§r§f This tag is locked!");
                                 return true;
                             }else{
@@ -117,7 +117,7 @@ class Main extends PluginBase {
                             return true;
                 case 5: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.women")) {
+                            if (!$player->hasPermission("tags.Rise")) {
                                 $player->sendMessage("§7§l(§c!§7)§r§f This tag is locked!");
                                 return true;
                             }else{
@@ -143,7 +143,7 @@ class Main extends PluginBase {
                             return true;
                 case 7: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.quickied")) {
+                            if (!$player->hasPermission("tags.YouTuber")) {
                                 $player->sendMessage("§7(§c!§7)§r§c This tag is locked!");
                                 return true;
                             }else{
@@ -157,7 +157,7 @@ class Main extends PluginBase {
  
                 case 8: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.twpe")) {
+                            if (!$player->hasPermission("tags.Mesa")) {
                                 $player->sendMessage("§7(§c!§7)§c This tag is locked!");
                                 return true;
                             }else{
@@ -170,7 +170,7 @@ class Main extends PluginBase {
                             return true;
 				case 9: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.aussieping")) {
+                            if (!$player->hasPermission("tags.Big Dilf")) {
                                 $player->sendMessage("§7(§c!§7)§c This tag is locked!");
                                 return true;
                             }else{
@@ -183,7 +183,7 @@ class Main extends PluginBase {
                             return true;			
 				case 10: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.ed")) {
+                            if (!$player->hasPermission("tags.Brac")) {
                                 $player->sendMessage("§7(§c!§7)§c This tag is locked!");
                                 return true;
                             }else{
@@ -209,7 +209,7 @@ class Main extends PluginBase {
                             return true;
                     case 12: // Second button (to second page)
 
-                            if (!$player->hasPermission("tags.100k")) {
+                            if (!$player->hasPermission("tags.Slayer")) {
                                 $player->sendMessage("§7(§c!§7)§c This tag is locked!");
                                 return true;
                             }else{
@@ -230,29 +230,29 @@ class Main extends PluginBase {
 		
         $form->addButton($player->hasPermission("tags.noob") === true ? "§r§l§6#§eNoob§r\n§r§aUNLOCKED" : "§r§l§6#§eNoob§r\n§r§cLOCKED"); 
 		
-		$form->addButton($player->hasPermission("tags.alien") === true ? "§l§2#§aA§2l§ai§2e§an§r§7§r\n§r§aUNLOCKED" : "§l§2#§aA§2l§ai§2e§an§r§7§r\n§r§cLOCKED");
+		$form->addButton($player->hasPermission("tags.God") === true ? "§l§2#§aA§2l§ai§2e§an§r§7§r\n§r§aUNLOCKED" : "§l§2#§aA§2l§ai§2e§an§r§7§r\n§r§cLOCKED");
 		
-        $form->addButton($player->hasPermission("tags.bombom") === true ? "§l§4#§cBo§4mB§com§r§7§r\n§r§aUNLOCKED" : "§l§4#§cBo§4mB§com§r§7§r\n§r§cLOCKED");
+        $form->addButton($player->hasPermission("tags.Bad") === true ? "§l§4#§cBo§4mB§com§r§7§r\n§r§aUNLOCKED" : "§l§4#§cBo§4mB§com§r§7§r\n§r§cLOCKED");
 		
 		$form->addButton($player->hasPermission("tags.hacker") === true ? "§l§3#§bHa§3ck§ber§r§7§r\n§r§aUNLOCKED" : "§l§3#§bHa§3ck§ber§r§7§r\n§r§cLOCKED"); 
 		
         $form->addButton($player->hasPermission("tags.spicy") === true ? "§r§r§l#§l§4S§cp§4i§cc§4y§r\n§r§aUNLOCKED" : "§r§r§l#§l§4S§cp§4i§cc§4y§r\n§r§cLOCKED");
 		
-        $form->addButton($player->hasPermission("tags.women") === true ? "§r§8[§n§dW§5o§dm§5e§dn§r§8]§r\n§r§aUNLOCKED" : "§r§8[§n§dW§5o§dm§5e§dn§r§8]§r\n§r§cLOCKED");
+        $form->addButton($player->hasPermission("tags.Rise") === true ? "§r§8[§n§dW§5o§dm§5e§dn§r§8]§r\n§r§aUNLOCKED" : "§r§8[§n§dW§5o§dm§5e§dn§r§8]§r\n§r§cLOCKED");
 		
         $form->addButton($player->hasPermission("tags.og") === true ? "§r§2#§aOG§r\n§r§aUNLOCKED" : "§r§2#§aOG§r\n§r§cLOCKED");
 		
-        $form->addButton($player->hasPermission("tags.quickied") === true ? "§r§8[§6Q§fu§6i§fc§6k§fi§6e§fd§8]§r\n§r§aUNLOCKED" : "§r§8[§6Q§fu§6i§fc§6k§fi§6e§fd§8]§r\n§r§cLOCKED");
+        $form->addButton($player->hasPermission("tags.YouTuber") === true ? "§r§8[§6Q§fu§6i§fc§6k§fi§6e§fd§8]§r\n§r§aUNLOCKED" : "§r§8[§6Q§fu§6i§fc§6k§fi§6e§fd§8]§r\n§r§cLOCKED");
 
-        $form->addButton($player->hasPermission("tags.twpe") === true ? "§r§6#§l§eT§6Warps§r\n§r§aUNLOCKED" : "§r§6#§l§eT§6Warps§r\n§r§cLOCKED");
+        $form->addButton($player->hasPermission("tags.Mesa") === true ? "§r§6#§l§eT§6Warps§r\n§r§aUNLOCKED" : "§r§6#§l§eT§6Warps§r\n§r§cLOCKED");
 		
-		$form->addButton($player->hasPermission("tags.aussieping") === true ? "§r§l§3#§r§aAussie§dPing§r\n§r§aUNLOCKED" : "§r§l§3#§r§aAussie§dPing§r\n§r§cLOCKED");
+		$form->addButton($player->hasPermission("tags.Big Dilf") === true ? "§r§l§3#§r§aAussie§dPing§r\n§r§aUNLOCKED" : "§r§l§3#§r§aAussie§dPing§r\n§r§cLOCKED");
 		
-        $form->addButton($player->hasPermission("tags.ed") === true ? "§r§l§5#§dE§5D§r\n§r§aUNLOCKED" : "§r§l§5#§dE§5D§r\n§r§cLOCKED");
+        $form->addButton($player->hasPermission("tags.Brac") === true ? "§r§l§5#§dE§5D§r\n§r§aUNLOCKED" : "§r§l§5#§dE§5D§r\n§r§cLOCKED");
 
         $form->addButton($player->hasPermission("tags.fearless") === true ? "§r§8[§l§4Fear§cless§r§8]§r\n§r§aUNLOCKED" : "§r§8[§l§4Fear§cless§r§8]§r\n§r§cLOCKED");
         
-		$form->addButton($player->hasPermission("tags.100k") === true ? "§r§8[§c100§4k§r§8]§r\n§r§aUNLOCKED" : "§r§8[§c100§4k§r§8]§r\n§r§cLOCKED");
+		$form->addButton($player->hasPermission("tags.Slayer") === true ? "§r§8[§c100§4k§r§8]§r\n§r§aUNLOCKED" : "§r§8[§c100§4k§r§8]§r\n§r§cLOCKED");
         
         
         $form->addButton("§c§lClose");
